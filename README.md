6134f1997917a0f45011a058196c935c167e61dc
MasonMefford <mason.mefford@uky.edu>
2025-04-29 11:51:44 -0400

    Commit all

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Final_Project
+[Robinson Forest Pointcloud](potree)
---
+++ potree/pointclouds/index/cloud.js
@@ -0,0 +1,91 @@
+{
+    "version": "1.8",
+    "octreeDir": "data",
+    "projection": "",
+    "points": 2529525,
+    "boundingBox": {
+        "lx": 5672742.0,
+        "ly": 3703388.5500000005,
+        "lz": 1086.63,
+        "ux": 5674241.97,
+        "uy": 3704888.52,
+        "uz": 2586.5999999997393
+    },
+    "tightBoundingBox": {
+        "lx": 5672742.0,
+        "ly": 3703388.5500000005,
+        "lz": 1086.63,
+        "ux": 5674241.96,
+        "uy": 3704888.52,
+        "uz": 1652.08
+    },
+    "pointAttributes": [
+        {
+            "name": "POSITION_CARTESIAN",
+            "size": 12,
+            "elements": 3,
+            "elementSize": 4,
+            "type": "int32",
+            "description": ""
+        },
+        {
+            "name": "RGBA",
+            "size": 4,
+            "elements": 4,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        },
+        {
+            "name": "classification",
+            "size": 1,
+            "elements": 1,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        },
+        {
+            "name": "gps-time",
+            "size": 8,
+            "elements": 1,
+            "elementSize": 8,
+            "type": "double",
+            "description": ""
+        },
+        {
+            "name": "intensity",
+            "size": 2,
+            "elements": 1,
+            "elementSize": 2,
+            "type": "uint16",
+            "description": ""
+        },
+        {
+            "name": "number of returns",
+            "size": 1,
+            "elements": 1,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        },
+        {
+            "name": "return number",
+            "size": 1,
+            "elements": 1,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        },
+        {
+            "name": "source id",
+            "size": 2,
+            "elements": 1,
+            "elementSize": 2,
+            "type": "uint16",
+            "description": ""
+        }
+    ],
+    "spacing": 12.990120887756348,
+    "scale": 0.001,
+    "hierarchyStepSize": 5
+}
